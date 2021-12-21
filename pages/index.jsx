9c083ee318d4589a7d116236656740a87cecb732
Juan Gomes Macário <jgomesmacario@gmail.com>
2021-12-21 02:11:04 -0300

    desktop to index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,13 @@
-export default function Home() {
-  return (
-    <div>
-    </div>
-  )
+import Taskbar from '../components/taskbar';
+import Menu from '../components/menu';
+
+const Desktop = () => {
+    return (
+        <div className="w-screen h-screen bg-windows-wallpaper bg-center bg-cover relative">
+            <Menu></Menu>
+            <Taskbar></Taskbar>
+        </div>
+    );
 }
+
+export default Desktop;
